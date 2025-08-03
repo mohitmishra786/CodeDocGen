1eb77d30bab01b326ede781d99c5dfb032356ddd
Mohit Mishra <immadmohit@gmail.com>
2025-08-03 15:43:39 +0530

    chore: Bump version to 1.1.0 for first PyPI release
    - First production release with intelligent comment generation
    - AST analysis and NLTK-powered documentation
    - Context-aware parameter and return type descriptions
    - Complete function coverage with intelligent comments

--- code_doc_gen/__init__.py
+++ code_doc_gen/__init__.py
@@ -13,7 +13,7 @@ from .scanner import RepositoryScanner
 from .config import Config
 from .generator import DocumentationGenerator
 
-__version__ = "1.0.18"
+__version__ = "1.1.0"
 __author__ = "Mohit Mishra"
 __license__ = "MIT"
 
